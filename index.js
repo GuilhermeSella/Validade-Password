47d018f3d55665e21485e0cccb57485acac40120
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-03 23:22:12 -0300

    Testando rotas e finalizando testes automatizados

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ app.listen(port, (error)=>{
     console.log("Servidor rodando na porta " + port )
 
 })
+
+module.exports = app
--- tests/validate-pass.test.js
+++ tests/validate-pass.test.js
@@ -1,6 +1,8 @@
 const ValidatePassword = require("../services/validate-pass")
+const request = require("supertest")
+const app = require("../index")
 
-describe("Validate password", ()=>{
+describe("Validando método validate password", ()=>{
 
     test("deve possuir ao menos 8 caracteres", ()=>{
         expect(ValidatePassword("gui")).toStrictEqual({
@@ -25,3 +27,29 @@ describe("Validate password", ()=>{
     })
 
 })
+
+
+describe("Validando rota validate-password", ()=>{
+    test("deve retornar statuscode 200 e senha forte", async()=>{
+        const response = await request(app).post("/validate-password").send({
+            password : "Guilherme1$"
+        })
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual({
+            "Senha forte": "Guilherme1$"
+        })
+    })
+
+
+    test("deve retornar statuscode 400 e senha fraca", async()=>{
+        const response = await request(app).post("/validate-password").send({
+            password : "Guilherme"
+        })
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toEqual({
+            error: "A senha deve conter no mínimo uma caracter especial(#,$,%,@) e um número",
+        })
+    })
+
+
+}) 
